81f0e04990b4b56d8d1b5a300075c710bb5d1af1
retterrudi <paul.serdack@gmail.com>
2025-04-14 13:50:42 +0200

    Move Logic for WarcraftlogsClient to separate file

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using System.Net.Http.Headers;
 using System.Text;
+using Cooldown_Usage_Comparator;
 using Cooldown_Usage_Comparator.Pages;
 using Cooldown_Usage_Comparator.Pages.models;
 using Newtonsoft.Json;
@@ -24,24 +25,5 @@ if (tokenResponse?.accessToken is null)
     return;
 }
 
-var _httpClient = new HttpClient
-{
-    BaseAddress = new Uri("https://www.warcraftlogs.com")
-};
-
-var apiUrl = "/api/v2/client";
-var authHeader = new AuthenticationHeaderValue("Bearer", tokenResponse.accessToken);
-
-var body = "{\"query\": \"query ReportData($code: String!, $fightID: Int!) { reportData { report(code: $code) { playerDetails(fightIDs: [$fightID]) } } }\",\"variables\": { \"code\": \"NbJGzkjLPtThAc4W\", \"fightID\": 1 } }";
-var content = new StringContent(body, Encoding.UTF8, "application/json");
-
-var request = new HttpRequestMessage(HttpMethod.Post, apiUrl)
-{
-    Headers = { Authorization = authHeader},
-    Content = content
-};
-
-var apiResponse = await _httpClient.SendAsync(request);
-apiResponse.EnsureSuccessStatusCode();
-PrettyPrintJson(await apiResponse.Content.ReadAsStringAsync());
-
+var warcraftLogsClient = new WarcraftlogsClient(tokenResponse.accessToken);
+await warcraftLogsClient.GetPlayerDetails("NbJGzkjLPtThAc4W", 1);
---
+++ WarcraftlogsClient.cs
@@ -0,0 +1,68 @@
+﻿using System.Net.Http.Headers;
+using System.Text;
+using static Cooldown_Usage_Comparator.utils.GeneralUtils;
+
+namespace Cooldown_Usage_Comparator;
+
+public class WarcraftlogsClient
+{
+    private readonly HttpClient _httpClient;
+    private readonly AuthenticationHeaderValue _authHeader;
+    private const string ApiUri = "";
+
+    // ReSharper disable once ConvertToPrimaryConstructor
+    public WarcraftlogsClient(string authToken)
+    {
+        _httpClient = new HttpClient
+        {
+            BaseAddress = new Uri("https://www.warcraftlogs.com/api/v2/client")
+        };
+        _authHeader = new AuthenticationHeaderValue("Bearer", authToken);
+    }
+
+    public async Task GetPlayerDetails(string fightCode, int fightId)
+    {
+        try
+        {
+            const string query = @"
+                query ReportData($code: String!, $fightId: Int!) {
+                    reportData {
+                        report (code: $code) {
+                            playerDetails (fightIDs: [$fightId])
+                        }
+                    }
+                }";
+
+            var body = new
+            {
+                query,
+                variables = new
+                {
+                    code = fightCode,
+                    // ReSharper disable once RedundantAnonymousTypePropertyName
+                    fightId = fightId
+                }
+            };
+            
+            var content = new StringContent(
+                System.Text.Json.JsonSerializer.Serialize(body), 
+                Encoding.UTF8, 
+                "application/json");
+            
+            var request = new HttpRequestMessage(HttpMethod.Post, ApiUri)
+            {
+                Headers = { Authorization = _authHeader },
+                Content = content
+            };
+
+            var apiResponse = await _httpClient.SendAsync(request);
+            apiResponse.EnsureSuccessStatusCode();
+            PrettyPrintJson(apiResponse.Content.ReadAsStringAsync().Result);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            // TODO
+        }
+    }
+}
