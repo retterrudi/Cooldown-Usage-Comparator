6910ec616970b00e2a10e96de48f862b185ab0a2
retterrudi <paul.serdack@gmail.com>
2025-05-15 15:52:44 +0200

    Add stub to fetch data from the backend

---
+++ frontend/src/types/types.ts
@@ -0,0 +1,11 @@
+﻿export interface Fight {
+  Difficulty: number | null;
+  EncounterId: number | null;
+  EndTime: number | null;
+  FriendlyPlayers: number[] | null;
+  Id: number | null;
+  KeystoneBonus: number | null;
+  KeystoneLevel: number | null;
+  Name: string | null;
+  StartTime: number | null;
+}
